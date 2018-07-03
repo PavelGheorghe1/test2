1f8f4556edd51febbfb0650f01a41c4983488051
Pavel Gheorghe <pavel.gheorghe@outlook.com>
2018-07-03 16:07:45 +0300

    first commit in 1_branch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,11 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
-            Console.WriteLine("hello");
-            Console.WriteLine("hello");
-            Console.WriteLine("hello");
-            Console.WriteLine("hello");
+            Console.WriteLine("hello1");
+            Console.WriteLine("hello1");
+            Console.WriteLine("hello1");
+            Console.WriteLine("hello1");
+            Console.WriteLine("hello1");
         }
     }
 }
