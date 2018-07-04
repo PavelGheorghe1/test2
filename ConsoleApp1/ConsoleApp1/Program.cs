e6189b4d68089b359008cada2f10896659482401
Pavel Gheorghe <pavel.gheorghe@outlook.com>
2018-07-04 11:11:34 +0300

    first commit in 3_branch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,6 +19,8 @@ namespace ConsoleApp1
             Console.WriteLine("hello13");
             Console.WriteLine("hello");
             Console.WriteLine("hello");
+            trewtrwetw
+                tyreyer
 
         }
     }
